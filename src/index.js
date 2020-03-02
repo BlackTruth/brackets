b9f5a926a10665458d200a7b28f94e3cd856892e
Kanstantsin Kulahin <blacktruth1991@gmail.com>
2020-03-02 22:26:18 +0300

    feat: optimize solution

--- src/index.js
+++ src/index.js
@@ -1,27 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-    let arr = str.split("");
     let stack = [];
-    for (let symbol of arr) {
-        if (isOpenBracket(symbol, stack[stack.length - 1], bracketsConfig)) {
+    for (let symbol of str.split("")) {
+        let last = stack.pop();
+        if (!isPair(symbol, last, bracketsConfig)) {
+            if (last) stack.push(last);
             stack.push(symbol);
-        } else if (!checkBrackets(stack.pop(), symbol, bracketsConfig))
-            return false;
+        }
     }
     return stack.length == 0;
 };
 
-function checkBrackets(bracketOpen, bracketClose, bracketsConfig) {
+function isPair(bracket, prevBracket, bracketsConfig) {
     for (let elem of bracketsConfig) {
-        if (bracketOpen == elem[0] && bracketClose == elem[1]) return true;
-    }
-    return false;
-}
-
-function isOpenBracket(bracket, prevBracket, bracketsConfig) {
-    for (let elem of bracketsConfig) {
-        if (elem[0] == bracket && elem[0] != elem[1]) return true;
-        if (elem[0] == bracket && elem[0] == elem[1] && bracket != prevBracket)
-            return true;
+        if (elem[0] == prevBracket && elem[1] == bracket) return true;
     }
     return false;
 }
