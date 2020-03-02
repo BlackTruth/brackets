5709fb29274ccd8b6c0bc6311ee9e6f64206adc5
Kanstantsin Kulahin <blacktruth1991@gmail.com>
2020-03-02 22:16:57 +0300

    init: brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arr = str.split("");
+    let stack = [];
+    for (let symbol of arr) {
+        if (isOpenBracket(symbol, stack[stack.length - 1], bracketsConfig)) {
+            stack.push(symbol);
+        } else if (!checkBrackets(stack.pop(), symbol, bracketsConfig))
+            return false;
+    }
+    return stack.length == 0;
+};
+
+function checkBrackets(bracketOpen, bracketClose, bracketsConfig) {
+    for (let elem of bracketsConfig) {
+        if (bracketOpen == elem[0] && bracketClose == elem[1]) return true;
+    }
+    return false;
+}
+
+function isOpenBracket(bracket, prevBracket, bracketsConfig) {
+    for (let elem of bracketsConfig) {
+        if (elem[0] == bracket && elem[0] != elem[1]) return true;
+        if (elem[0] == bracket && elem[0] == elem[1] && bracket != prevBracket)
+            return true;
+    }
+    return false;
 }
